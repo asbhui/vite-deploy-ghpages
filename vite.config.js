dcd07027f42faa0c9e92bf0679c6fffae3c9d06d
Arvinder Singh <asbhui@gmail.com>
2025-02-02 01:13:20 +0000

    working with dockerfile and basename

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${packageJson.basename}/`,
+  build: {
+    outDir: `dist/${packageJson.basename}/`
+  },
   plugins: [react()],
 });
